f57f85772d24c8360d7b54cafaa44bff534dde44
buraksecer <brkscr@hotmail.com.tr>
2019-12-05 12:33:19 +0300

    Get By Id Request V1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-const API_URL = 'http://jsonplaceholder.typicode.com';
-  
+const API_URL = 'http://jsonplaceholder.typicode.com'; 
 class App extends Component {
 
   state = {
-    users: []
+    users: [],
+    id: ''
   }
   
   componentDidMount() {
-    
+    //This zone is loading before page render
   }
  
-  handleClick = () => {
+  handleGetAllDataClick = () => {
     const url = `${API_URL}/users/`;
     axios.get(url).then(response => response.data)
     .then((data) => {
@@ -20,12 +20,28 @@ class App extends Component {
      })
   }
 
+  handleChange = event => {
+    this.setState({ id: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();  
+     console.log(`${this.state.id}`); 
+  }
+
   render() {
     return (
       <div className="container">
-      <button onClick={this.handleClick}>
+         <form onSubmit={this.handleSubmit}>
+          <label>
+            Person ID:
+            <input type="text" name="id" onChange={this.handleChange} />
+          </label>
+          <button type="submit">GET BY ID</button>
+        </form>
+      <button onClick={this.handleGetAllDataClick}>
         GET DATAS
-      </button>
+      </button> 
       <div className="col-xs-8">
       <h1>React Axios Example</h1>
       {this.state.users.map((user) => (
